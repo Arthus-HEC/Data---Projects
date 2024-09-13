b350e434c8eae4f9e191a899460addc11380850c
Arthus-HEC <Arthus.goujon@hec.edu>
2024-09-13 14:16:21 +0200

    Add files via upload
    European Option Pricing with Monte Carlo Simulation
    This Python script demonstrates how to price a European option using Monte Carlo simulation. The script performs the following tasks:
    
    Imports necessary packages: numpy, yfinance, and datetime.
    Downloads historical stock data: Fetches the past year of data for a specified stock using yfinance.
    Computes stock volatility: Calculates the standard deviation of daily returns from the adjusted closing prices.
    Defines European option parameters: Sets up parameters for the option pricing model, including stock price, strike price, risk-free rate, and maturity.
    Calculates option price: Uses a Monte Carlo simulation to estimate the price of a European call or put option based on the provided parameters.
    Features:
    
    Calculates stock volatility from historical data.
    Supports European call and put options.
    Utilizes Monte Carlo simulation for option pricing.
    Usage: Modify the stock_name, strike_price, risk_free_rate, and other parameters to price different options. The script is currently set to price a put option for Apple Inc. (AAPL).
    
    Dependencies:
    
    numpy
    yfinance

---
+++ Princing_Option_MC.py
@@ -0,0 +1,53 @@
+## Import packages 
+
+import numpy as np
+import yfinance as yf
+from datetime import datetime, timedelta
+
+## Download market data 
+
+stock_name = 'AAPL'
+end_date = datetime.today()
+start_date = end_date - timedelta(days = 365)
+
+stock_data = yf.download(
+    stock_name, 
+    start = start_date, 
+    end = end_date)
+
+## Compute stock volatility 
+
+d_returns = stock_data['Adj Close'].pct_change(1)
+stock_vol = d_returns.std()
+
+
+## European option parameters
+
+stock_price = stock_data['Adj Close'][-1]
+strike_price = stock_price # At the money
+
+risk_free_rate = 0.05
+maturity = 1
+
+## European option with MC
+
+def european_option_price(S0, K, v, r, T, nSim, flag):
+    z = np.random.standard_normal(nSim)
+    S_fwd = S0 * np.exp((r-0.5* v**2)*T + v*np.sqrt(T)*z)
+    
+    if flag == 'call':
+        payoff = np.maximum(S_fwd - K, 0)
+    elif flag == 'put':
+        payoff = np.maximum(K-S_fwd, 0)
+    else :
+        print("incorrect flag")
+        return 
+    
+    return np.exp(-r*T)*np.sum(payoff)/nSim
+
+nSim = 100000
+option_price = european_option_price(
+    stock_price, strike_price, stock_vol, risk_free_rate, maturity, nSim, 'put')
+
+    
+
